a4bd53c6bb1b35a622803675a7d16139a3bb405d
ugurbayiroglu-sudo <ugurbayiroglu@gmail.com>
2026-02-04 15:26:18 +0300

    Implement authentication guard for admin access

--- js/guard.js
+++ js/guard.js
@@ -1,1 +1,13 @@
+// js/guard.js
+import { auth } from "./firebase.js";
+import { onAuthStateChanged } from
+  "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
+
+const ADMIN_EMAIL = "ugurbayiroglu@gmail.com";
+
+onAuthStateChanged(auth, (user) => {
+  if (!user || user.email !== ADMIN_EMAIL) {
+    window.location.href = "login.html";
+  }
+});
 
