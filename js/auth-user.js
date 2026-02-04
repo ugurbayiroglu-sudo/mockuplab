d4939005ca8f782408c52a497c6556d1c759fb1d
ugurbayiroglu-sudo <ugurbayiroglu@gmail.com>
2026-02-04 15:28:58 +0300

    Implement Google login with Firebase authentication

--- js/auth-user.js
+++ js/auth-user.js
@@ -1,1 +1,15 @@
+import { auth } from "./firebase.js";
+import { GoogleAuthProvider, signInWithPopup } from
+  "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
+
+const provider = new GoogleAuthProvider();
+
+document.getElementById("googleLogin").addEventListener("click", async () => {
+  try {
+    await signInWithPopup(auth, provider);
+    alert("Welcome!");
+  } catch (e) {
+    console.error(e);
+  }
+});
 
