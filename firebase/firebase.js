b730d83826da915c9c1ac4942014efcffd97723c
ugurbayiroglu-sudo <ugurbayiroglu@gmail.com>
2026-02-04 15:20:46 +0300

    Refactor Firebase initialization and exports
    Updated Firebase initialization and export structure.

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,8 +1,6 @@
-// firebase/firebase.js
-
+// js/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDyr9GGkgPvSK0RFpb5cuo1Q_RReywul7Y",
@@ -13,10 +11,6 @@ const firebaseConfig = {
   appId: "1:1046934952879:web:3785577abff8c69d1bed5a"
 };
 
-// Firebase başlat
-const app = initializeApp(firebaseConfig);
-
-// Servisleri dışa aktar
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
 
